9f1c134b200241fbbc2f8724f022daacca0023e8
poojareddy3 <poojithavangala@gmail.com>
2022-04-05 16:32:24 -0400

    all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { Route, Routes, useLocation } from 'react-router-dom'
 import './App.css';
+import Home from './components/routes/Home'
+import Items from './components/routes/Items'
+import ItemCreate from './components/routes/ItemCreate'
+import Item from './components/routes/Item'
+import ItemEdit from './components/routes/ItemEdit'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* With complex applications, sometimes we will need to access the history of the objects closest Route match */}
+      {/* in Routerv5 it had withRouter, deprecated in v6. */}
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/items' element={<Items />} />
+          <Route path='/create-item' element={<ItemCreate />} />
+          <Route path='/items/:id' element={<Item />} />
+          <Route path='/items/:id/edit' element={<ItemEdit />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+// function withRouter(Child){
+//    return function withRouter(props){
+//      const location = useLocation()
+//      return <Child { ...props} location={location} />
+//    }
+// }
+
+export default App
