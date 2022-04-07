47243add1a9b292476edf174986a6404ee8ac4a6
JoyH127 <hh495@scarletmail.rutgers.edu>
2022-04-07 12:40:08 -0400

    finished createdItem

--- src/components/routes/Item.js
+++ src/components/routes/Item.js
@@ -1,58 +1,60 @@
-import { useState, useEffect } from 'react'
-import { NavLink, useNavigate, useParams } from 'react-router-dom'
-import axios from 'axios'
-import Layout from '../shared/Layout'
+import { useState, useEffect } from "react";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import Layout from "../shared/Layout";
+import apiUrl from "../../apiConfig";
+function Item() {
+  const [item, setItem] = useState([]);
+  const [deleted, setDeleted] = useState(false);
+  const { id } = useParams();
+  // console.log('params', id)
+  let navigate = useNavigate();
 
-function Item(){
-    const [item, setItem] = useState([])
-    const [deleted, setDeleted] = useState(false)
-    const { id } = useParams()
-    // console.log('params', id)
-    let navigate = useNavigate()
-
-useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-        try {
-          const response = await axios(`http://localhost:3000/api/items/${id}`)
-          console.log(response)
-          setItem(response.data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    fetchData()
-},[])
+      try {
+        const response = await axios(`${apiUrl}/items/${id}`);
+        console.log(response);
+        setItem(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-useEffect(() => {
-    if(!item) {
-        return <p>Loading...</p>
+  useEffect(() => {
+    if (!item) {
+      return <p>Loading...</p>;
     }
-}, [item])
+  }, [item]);
 
-const destroy = () => {
+  const destroy = () => {
     axios({
-        url: `http://localhost:3000/api/items/${id}`,
-        method: 'DELETE'
-    }).then(() => setDeleted(true)).catch(console.error)
+      url: `${apiUrl}/items/${id}`,
+      method: "DELETE",
+    })
+      .then(() => setDeleted(true))
+      .catch(console.error);
+  };
+
+  useEffect(() => {
+    if (deleted) {
+      return navigate("/");
+    }
+  }, [deleted, navigate]);
+
+  return (
+    <Layout>
+      <h4>{item.title}</h4>
+      <p>Link:{item.link}</p>
+      <button onClick={() => destroy()}>Delete Item</button>
+      <NavLink to={`/items/${id}/edit`}>
+        <button>Edit</button>
+      </NavLink>
+      <NavLink to="/items">Back to all items</NavLink>
+    </Layout>
+  );
 }
 
-useEffect(() =>{
-     if(deleted){
-         return navigate("/")
-     }
-}, [deleted,navigate])
-
-    return(
-        <Layout>
-           <h4>{item.title}</h4>
-           <p>Link:{item.link}</p>
-           <button onClick={() => destroy()}>Delete Item</button>
-           <NavLink to={`/items/${id}/edit`}>
-               <button>Edit</button>
-           </NavLink>
-           <NavLink to="/items">Back to all items</NavLink>
-        </Layout>
-    )
-}
-
-export default Item
+export default Item;
--- src/components/routes/ItemEdit.js
+++ src/components/routes/ItemEdit.js
@@ -1,12 +1,81 @@
+// import { useState, useEffect } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
+// import axios from "axios";
+// import Layout from "../shared/Layout";
+// import ItemForm from "../shared/ItemForm";
+
+// function ItemEdit() {
+//   const navigate = useNavigate();
+//   const { id } = useParams(); //get the id from the current object to update
+//   const [item, setItem] = useState({
+//     title: "",
+//     link: "",
+//   });
+//   const [updated, setUpdated] = useState(false);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios(`http://localhost:3000/api/items/${id}`);
+//         console.log("itemEdit", response);
+//         setItem(response.data);
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   const handleChange = (event) => {
+//     //created a placeholder grabbing the value from the user input form
+//     const updatedField = { [event.target.name]: event.target.value };
+//     //assigned the empty state with the updatedField into one object
+//     const editedItem = Object.assign(item, updatedField);
+//     //assigned the new object to be updated to the state
+//     setItem(editedItem);
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     axios({
+//       url: `http://localhost:3000/api/items/${id}`,
+//       method: "PUT",
+//       data: item,
+//     })
+//       .then(() => setUpdated(true))
+//       .catch(console.error);
+//   };
+
+//   useEffect(() => {
+//     if (updated) {
+//       return navigate(`/items/${id}`);
+//     }
+//   }, []);
+
+//   return (
+//     <Layout>
+//       <ItemForm
+//         item={item}
+//         handleChange={(e) => handleChange(e)}
+//         handleSubmit={(e) => handleSubmit(e)}
+//         cancelPath={`/items/${id}`}
+//       />
+//     </Layout>
+//   );
+// }
+
+// export default ItemEdit;
+
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import Layout from "../shared/Layout";
 import ItemForm from "../shared/ItemForm";
-
+import apiUrl from "../../apiConfig";
 function ItemEdit() {
   const navigate = useNavigate();
-  const { id } = useParams(); //get the id from the current object to update
+  const { id } = useParams(); // get the id from the current object
   const [item, setItem] = useState({
     title: "",
     link: "",
@@ -16,30 +85,31 @@ function ItemEdit() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios(`http://localhost:3000/api/items/${id}`);
+        const response = await axios(`${apiUrl}/items/${id}`);
         console.log("itemEdit", response);
         setItem(response.data);
-      } catch (err) {
-        console.error(err);
+      } catch (error) {
+        console.error(error);
       }
     };
+
     fetchData();
   }, []);
 
   const handleChange = (event) => {
-    //created a placeholder grabbing the value from the user input form
     const updatedField = { [event.target.name]: event.target.value };
-    //assigned the empty state with the updatedField into one object
+    // created a placeholder grabbing the value from the user input form
     const editedItem = Object.assign(item, updatedField);
-    //assigned the new object to be updated to the state
+    // assigned the empty state with the updatedField into one object
     setItem(editedItem);
+    // assigned the new object to be updated to the state
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     axios({
-      url: `http://localhost:3000/api/items/${id}`,
+      url: `${apiUrl}/items/${id}`,
       method: "PUT",
       data: item,
     })
@@ -64,5 +134,4 @@ function ItemEdit() {
     </Layout>
   );
 }
-
 export default ItemEdit;
